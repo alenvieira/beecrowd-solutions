91388a829301aa768b4a4e6adad11767892584c6
Alen Vieira <alencv@gmail.com>
2021-07-11 19:13:53 -0300

    add problem 1008

---
+++ 1-beginner/1008.py
@@ -0,0 +1,7 @@
+number_employee = int(input(''))
+hours = int(input())
+value_work_hour = float(input())
+salary = hours * value_work_hour
+print('NUMBER = {}'.format(number_employee))
+print('SALARY = U$ {:.2f}'.format(salary))
+
