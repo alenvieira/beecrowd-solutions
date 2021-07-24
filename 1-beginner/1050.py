fddd4b3fa63e4029bb34343f09b68bb63aff987e
Alen Vieira <alencv@gmail.com>
2021-07-23 22:34:40 -0300

    add problem 1050

---
+++ 1-beginner/1050.py
@@ -0,0 +1,19 @@
+ddd = int(input())
+state = "DDD nao cadastrado"
+if ddd == 61:
+    state = 'Brasilia'
+elif ddd == 71:
+    state = 'Salvador'
+elif ddd == 11:
+    state = 'Sao Paulo'
+elif ddd == 21:
+    state = 'Rio de Janeiro'
+elif ddd == 32:
+    state = 'Juiz de Fora'
+elif ddd == 19:
+    state = 'Campinas'
+elif ddd == 27:
+    state = 'Vitoria'
+elif ddd == 31:
+    state = 'Belo Horizonte'
+print(state)
