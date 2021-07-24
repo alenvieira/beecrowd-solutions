06d09f595a95e4052082b22a4782d962bf1cd2f0
Alen Vieira <alencv@gmail.com>
2021-07-23 22:28:32 -0300

    add problem 1049

---
+++ 1-beginner/1049.py
@@ -0,0 +1,19 @@
+subphylum = input()
+animal_class = input()
+animal_order = input()
+if subphylum == 'vertebrado' and animal_class == 'ave' and animal_order == 'carnivoro':
+    print('aguia')
+elif subphylum == 'vertebrado' and animal_class == 'ave' and animal_order == 'onivoro':
+    print('pomba')
+elif subphylum == 'vertebrado' and animal_class == 'mamifero' and animal_order == 'onivoro':
+    print('homem')
+elif subphylum == 'vertebrado' and animal_class == 'mamifero' and animal_order == 'herbivoro':
+    print('vaca')
+elif subphylum == 'invertebrado' and animal_class == 'inseto' and animal_order == 'hematofago':
+    print('pulga')
+elif subphylum == 'invertebrado' and animal_class == 'inseto' and animal_order == 'herbivoro':
+    print('lagarta')
+elif subphylum == 'invertebrado' and animal_class == 'anelideo' and animal_order == 'hematofago':
+    print('sanguessuga')
+elif subphylum == 'invertebrado' and animal_class == 'anelideo' and animal_order == 'onivoro':
+    print('minhoca')
