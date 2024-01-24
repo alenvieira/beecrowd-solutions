9b9e99fe96e2249840b0e4f822bfce1227cba943
Alen Vieira <alencv@gmail.com>
2024-01-24 11:56:53 -0300

    refactor problem 1048

--- 1-beginner/1048.py
+++ 1-beginner/1048.py
@@ -1,17 +1,17 @@
 salary =  float(input())
-if salary >= 0 and salary <= 400:
+if 0 <= salary <= 400:
     print('Novo salario: {:.2f}'.format(salary * 1.15))
     print('Reajuste ganho: {:.2f}'.format(salary * 0.15))
     print('Em percentual: 15 %')
-elif salary > 400 and salary <= 800:
+elif 400 < salary <= 800:
     print('Novo salario: {:.2f}'.format(salary * 1.12))
     print('Reajuste ganho: {:.2f}'.format(salary * 0.12))
     print('Em percentual: 12 %')
-elif salary > 800 and salary <= 1200:
+elif 800 < salary <= 1200:
     print('Novo salario: {:.2f}'.format(salary * 1.1))
     print('Reajuste ganho: {:.2f}'.format(salary * 0.1))
     print('Em percentual: 10 %')
-elif salary > 1200 and salary <= 2000:
+elif 1200 < salary <= 2000:
     print('Novo salario: {:.2f}'.format(salary * 1.07))
     print('Reajuste ganho: {:.2f}'.format(salary * 0.07))
     print('Em percentual: 7 %')
